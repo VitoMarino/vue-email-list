6305d65cdc20013ccf0bed9342f1be2f8ccfb6ed
VitoMarino <marino.vito2001@gmail.com>
2024-06-05 16:22:16 +0200

    Esercizio

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ const { createApp } = Vue
     createApp({
         data() {
             return {
-                
+                response : '',
+                mail: [
+
+                ]
             }
-        }
+        },
+        methods: {
+            getMail(){
+                for (let i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                    this.response = response.data.response;
+                    console.log(response.data);
+                    this.mail.push(this.response);
+                });
+                }
+            }
+        },
+
+        created(){
+            this.getMail();
+        },
     }).mount('#app')
